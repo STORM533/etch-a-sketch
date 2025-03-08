9cff58bd63ed553ac79daaca88848c860ad072c5
STORM533 <sakshamkr2006@gmail.com>
2025-03-09 01:53:29 +0530

    removed unnecessary classline

--- javaScript.js
+++ javaScript.js
@@ -2,13 +2,11 @@ function createDivs(input) {
     const container = document.querySelector("#container");
 
     for (let i = 1; i <= input; i++) {
-        const parentDiv = document.createElement("div");
-        parentDiv.classList.add("grid-vertical"); // Use class instead of ID
+        const parentDiv = document.createElement("div"); 
         parentDiv.classList.add("grids-verticals");
 
         for (let j = 1; j <= input; j++) {
             const childDiv = document.createElement("div");
-            childDiv.classList.add("grid-horizontal"); // Use class instead of ID
             childDiv.classList.add("grids-horizontals");
             parentDiv.appendChild(childDiv);
         }
