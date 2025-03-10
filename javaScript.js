063538e81b4fa49329d2cb68287b98550b6c50c5
STORM533 <sakshamkr2006@gmail.com>
2025-03-11 03:52:27 +0530

    added hover effect of random colors

--- javaScript.js
+++ javaScript.js
@@ -1,36 +1,55 @@
+function randomColors(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function createDivs(input) {
     const container = document.querySelector("#container");
 
     for (let i = 1; i <= input; i++) {
-        const parentDiv = document.createElement("div"); 
-        parentDiv.classList.add("grids-verticals");
+        const pDiv = document.createElement("div"); 
+        pDiv.classList.add("grids-verticals");
 
         for (let j = 1; j <= input; j++) {
-            const childDiv = document.createElement("div");
-            childDiv.classList.add("grids-horizontals");
-            parentDiv.appendChild(childDiv);
+            const cDiv = document.createElement("div");
+            cDiv.classList.add("grids-horizontals");
+            cDiv.addEventListener("mouseover" , function(){
+                this.style.backgroundColor = randomColors();
+            });
+            cDiv.addEventListener("mouseout" , function(){
+                this.style.backgroundColor = randomColors();
+            })
+
+            pDiv.appendChild(cDiv);
         }
 
-        container.appendChild(parentDiv);
+        container.appendChild(pDiv);
     }
 }
+
+
 const form = document.querySelector("#myForm");
 const input = document.querySelector("#name");
+    
+form.addEventListener("submit" , (event) => {
+    event.preventDefault();
+    const submitButton = event.submitter;
+    submitButton.disabled = true;
+    const gridSize = parseInt(input.value);
+    if (!isNaN(gridSize) && gridSize > 0) {
+        createDivs(gridSize);      
+        input.disabled = true;
+        alert("refresh page for more creation")
+    } else {
+        alert("refresh page and enter a number greater than 0.");
+    }
+});
 
-    form.addEventListener("submit" , (event) => {
-        event.preventDefault();
-        const submitButton = event.submitter;
-        submitButton.disabled = true;
-        const gridSize = parseInt(input.value);
-        if (!isNaN(gridSize) && gridSize > 0) {
-            createDivs(gridSize);
-            
-            input.disabled = true;
-            alert("refresh for more creation")
-        } else {
-            alert("Please enter a number greater than 0.");
-        }
-    });
+
 
 
 
